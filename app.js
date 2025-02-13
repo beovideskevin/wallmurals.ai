bc2cc3e1d5cbb9c333189818aab74917456ed9d4
Kevin B Casas <kevinbcasas@gmail.com>
2025-02-12 20:00:18 -0500

    routes

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', indexRouter);app.use('/ar', arRouter);
 app.use('/users', usersRouter);
-app.use('/ar', arRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
