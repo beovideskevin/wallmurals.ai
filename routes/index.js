f170af44b4353b0dafd58516654421c3b6a24c91
Kevin B Casas <kevinbcasas@gmail.com>
2025-02-13 23:45:00 -0500

    adding website

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: process.env.TITLE,
+    keywords: process.env.KEYWORDS,
+    description: process.env.DESCRIPTION,
+    author: process.env.AUTHOR,
+  });
 });
 
 module.exports = router;
