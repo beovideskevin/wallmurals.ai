5c0e114ea6ebf833e44895e15d6555e705b16a51
Kevin B Casas <kevinbcasas@gmail.com>
2025-03-04 22:06:35 -0500

    missing stuff

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const uglifycss = require('gulp-uglifycss');
+
+const paths = {
+    dist: {
+        css: {
+            dir: './public/dist/',
+        },
+        js: {
+            dir: './public/dist/',
+        },
+    },
+    src: {
+        css: {
+            files: './public/ar/*.css'
+        },
+        js: {
+            files: './public/ar/*.js'
+        }
+    }
+};
+
+gulp.task('js', function () {
+    return gulp.src(paths.src.js.files)
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.dist.js.dir));
+});
+
+gulp.task('css', function () {
+    return gulp.src(paths.src.css.files)
+        .pipe(uglifycss({
+            "maxLineLn": 80,
+            "uglyComments": true
+        }))
+        .pipe(gulp.dest(paths.dist.css.dir));
+});
+
+gulp.task('default', gulp.series(gulp.parallel('js', 'css')));
