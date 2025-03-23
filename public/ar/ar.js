1e9a675b2df7529d0c3964247ef7de6566a02a60
Kevin B Casas <kevinbcasas@gmail.com>
2025-03-22 22:23:03 -0400

    adding audio codec

--- public/ar/ar.js
+++ public/ar/ar.js
@@ -284,7 +284,7 @@ document.addEventListener('DOMContentLoaded', async function() {
     // Change the mime type for iPhone and safari
 
     mediaRecOptions = {
-        mimeType: "video/mp4;",
+        mimeType: "video/mp4;codecs=avc1,mp4a",
         // videoBitsPerSecond : 100000
     };
 
