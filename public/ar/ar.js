9c860b0347649cb54eca261ac0208661d1c87ccf
Kevin B Casas <kevinbcasas@gmail.com>
2025-03-22 22:13:28 -0400

    Test with mp4 no codec

--- public/ar/ar.js
+++ public/ar/ar.js
@@ -284,7 +284,7 @@ document.addEventListener('DOMContentLoaded', async function() {
     // Change the mime type for iPhone and safari
 
     mediaRecOptions = {
-        mimeType: "video/webm;",
+        mimeType: "video/mp4;",
         videoBitsPerSecond : 100000
     };
 
