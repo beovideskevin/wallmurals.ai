37d927fbf5fca33a02364f714f1a6eb2ddba7af5
Kevin B Casas <kevinbcasas@gmail.com>
2025-03-26 17:47:49 -0400

    30 frame rate

--- public/ar/ar.js
+++ public/ar/ar.js
@@ -12,7 +12,7 @@ var isMuted = true;
 var currentlyPlayingVideo = null;
 var currentlyPlayingAudio = null;
 // Recording stuff
-const frameRate = 24; // 30; // FPS
+const frameRate = 30; // FPS
 var recFrameId = null;
 var mediaRecorder = null;
 var canvas = null;
