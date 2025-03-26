82950653ea9072cec44785713e47cac8f1e73cb2
Kevin B Casas <kevinbcasas@gmail.com>
2025-03-26 15:05:20 -0400

    debugs mp4-muxer

--- public/ar/ar.js
+++ public/ar/ar.js
@@ -328,15 +328,7 @@ document.addEventListener('DOMContentLoaded', async function() {
     }
 
     canvas = document.getElementById('record');
-    if (window.innerWidth > window.innerHeight) {
-        // Optimal size for instagram, it could deform the image a little bit
-        canvas.width = 1920;
-        canvas.height = 1080;
-    }
-    else {
-        canvas.width = 1080;
-        canvas.height = 1920;
-    }
+    resizeCanvas()
     canvasContext = canvas.getContext('2d', {
         desynchronized: true
     });
@@ -453,18 +445,18 @@ document.addEventListener('DOMContentLoaded', async function() {
     document.getElementById("recVideoBtn").addEventListener('click', function() {
         audioCtx = audioCtx || new AudioContext();
         // if (videoMimeType === "video/webm") {
-            if (elements[0].audioElement) {
-                if (audioTrack === null) {
-                    source = audioCtx.createMediaElementSource(elements[0].audioElement);
-                    source.connect(audioCtx.destination);
-                }
-                destination = audioCtx.createMediaStreamDestination();
-                source.connect(destination);
-                audioTrack = destination.stream.getAudioTracks()[0];
-            }
-
-            let audioSampleRate = audioTrack?.getSettings().sampleRate;
-            let audioNumberOfChannels = audioTrack?.getSettings().channelCount;
+        //     if (elements[0].audioElement) {
+        //         if (audioTrack === null) {
+        //             source = audioCtx.createMediaElementSource(elements[0].audioElement);
+        //             source.connect(audioCtx.destination);
+        //         }
+        //         destination = audioCtx.createMediaStreamDestination();
+        //         source.connect(destination);
+        //         audioTrack = destination.stream.getAudioTracks()[0];
+        //     }
+        //
+        //     let audioSampleRate = audioTrack?.getSettings().sampleRate;
+        //     let audioNumberOfChannels = audioTrack?.getSettings().channelCount;
 
             // Create an MP4 muxer with a video track and maybe an audio track
             muxer = new Mp4Muxer.Muxer({
@@ -498,29 +490,29 @@ document.addEventListener('DOMContentLoaded', async function() {
                 bitrate: 1e6
             });
 
-            if (audioTrack) {
-                audioEncoder = new AudioEncoder({
-                    output: (chunk, meta) => muxer.addAudioChunk(chunk, meta),
-                    error: e => console.error(e)
-                });
-                audioEncoder.configure({
-                    codec: 'mp4a.40.2',
-                    numberOfChannels: audioNumberOfChannels,
-                    sampleRate: audioSampleRate,
-                    bitrate: 128000
-                });
-
-                // Create a MediaStreamTrackProcessor to get AudioData chunks from the audio track
-                let trackProcessor = new MediaStreamTrackProcessor({ track: audioTrack });
-                let consumer = new WritableStream({
-                    write(audioData) {
-                        if (!recording) return;
-                        audioEncoder.encode(audioData);
-                        audioData.close();
-                    }
-                });
-                trackProcessor.readable.pipeTo(consumer);
-            }
+            // if (audioTrack) {
+            //     audioEncoder = new AudioEncoder({
+            //         output: (chunk, meta) => muxer.addAudioChunk(chunk, meta),
+            //         error: e => console.error(e)
+            //     });
+            //     audioEncoder.configure({
+            //         codec: 'mp4a.40.2',
+            //         numberOfChannels: audioNumberOfChannels,
+            //         sampleRate: audioSampleRate,
+            //         bitrate: 128000
+            //     });
+            //
+            //     // Create a MediaStreamTrackProcessor to get AudioData chunks from the audio track
+            //     let trackProcessor = new MediaStreamTrackProcessor({ track: audioTrack });
+            //     let consumer = new WritableStream({
+            //         write(audioData) {
+            //             if (!recording) return;
+            //             audioEncoder.encode(audioData);
+            //             audioData.close();
+            //         }
+            //     });
+            //     trackProcessor.readable.pipeTo(consumer);
+            // }
 
             startTime = document.timeline.currentTime;
             lastKeyFrame = -Infinity;
@@ -591,7 +583,7 @@ document.addEventListener('DOMContentLoaded', async function() {
             copyRenderedCanvas(copyContext);
             resizeAndCopyCopy(copyCanvas);
             if (videoMimeType === "video/webm") {
-                // encodeVideoFrame();
+                encodeVideoFrame();
             }
         }, 1000 / frameRate);
     });
@@ -810,6 +802,18 @@ window.addEventListener("hashchange", function() {
 /**
  * Helpers for recording video
  */
+function resizeCanvas() {
+    if (window.innerWidth > window.innerHeight) {
+        // Optimal size for instagram, it could deform the image a little bit
+        canvas.width = 1920;
+        canvas.height = 1080;
+    }
+    else {
+        canvas.width = 1080;
+        canvas.height = 1920;
+    }
+}
+
 function copyRenderedCanvas(ctx)
 {
     const {video, renderer, scene, camera} = mindarThree;
