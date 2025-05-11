30c3c4f831064608314572bda2a45a624a576fd9
Kevin B Casas <kevinbcasas@gmail.com>
2025-05-11 17:21:29 -0400

    Control cache removes no-store

--- helpers/utils.js
+++ helpers/utils.js
@@ -279,7 +279,7 @@ function generateRandomFilename(length = 16) {
 }
 
 const noCacheRequest = function(req, res, next) {
-    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.set('Cache-Control', 'no-cache, must-revalidate, proxy-revalidate');
     res.set('Pragma', 'no-cache');
     res.set('Expires', '0');
     res.set('Surrogate-Control', 'no-store');
